2b9cc678aef9eb2e5b70451c3d481257c3610280
Sudharsan-Yadav <saisudharsan265@gmail.com>
2026-09-02 14:08:00 +0530

    Initialize database and create aws and azure tables

---
+++ Code/init.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE sudhan;
+USE sudhan;
+CREATE TABLE aws (
+    id INT PRIMARY KEY,
+    service_name VARCHAR(50)
+);
+
+CREATE TABLE azure (
+    id INT PRIMARY KEY,
+    service_name VARCHAR(50)
+);
